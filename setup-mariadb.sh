0e2dab47f0638fbc9d395310ee64186fa1461893
st10576 <st10576@iismarconipieralisi.it>
2025-04-29 07:29:26 +0000

    modificato nome container su script avvio

--- setup-mariadb.sh
+++ setup-mariadb.sh
@@ -20,7 +20,7 @@ sleep 10
 
 # Configura i permessi per l'accesso remoto a MariaDB 
 # attenzione al nome del container, verificarlo con "docker ps"
-docker exec -i docker-xammp-ws-db-1 mariadb -u root -proot <<EOF
+docker exec -i  scout-admin-db-1 mariadb -u root -proot <<EOF
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root';
 FLUSH PRIVILEGES;
 GRANT ALL PRIVILEGES ON *.* TO 'user'@'%' IDENTIFIED BY 'user';
@@ -29,8 +29,8 @@ EOF
 echo "MariaDB setup complete."
 
 # Importa lo schema e i dati del database
-docker exec -i docker-xammp-ws-db-1 mariadb -u root -proot <./db/dumps/schema.sql
+docker exec -i scout-admin-db-1 mariadb -u root -proot <./db/dumps/schema.sql
 echo "DB schema loaded."
 
-docker exec -i docker-xammp-ws-db-1 mariadb -u root -proot <./db/dumps/data.sql
+docker exec -i scout-admin-db-1 mariadb -u root -proot <./db/dumps/data.sql
 echo "DB populated."
