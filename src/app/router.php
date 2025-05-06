c3b92c82056446e6095f0ee6b0b4140b94666db8
st10583 <st10583@iismarconipieralisi.it>
2025-05-06 07:59:10 +0000

    insert/read/update account

--- src/app/router.php
+++ src/app/router.php
@@ -57,6 +57,83 @@ function gestisci_richiesta_non_valida() {
     echo "Risorsa non trovata";
 }
 
+function insert_account()
+{
+    global $conn;
+
+    if (isset($_POST['email']) && 
+        isset($_POST['password']) && 
+        isset($_POST['username']) && 
+        isset($_POST['id_persona'])) 
+    {
+        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+        $query = "INSERT INTO Account (email, password, username, id_persona) VALUES (:email, :password, :username, :id_persona)";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(':email', $_POST['email'], PDO::PARAM_STR);
+        $stmt->bindParam(':password', $password, PDO::PARAM_STR);
+        $stmt->bindParam(':username', $_POST['username'], PDO::PARAM_STR);
+        $stmt->bindParam(':id_persona', $_POST['id_persona'], PDO::PARAM_INT);
+        $stmt->execute();
+    
+        if ($stmt->rowCount() > 0) {
+            echo json_encode("success");
+            return;
+        }
+    }
+    echo json_encode("error");
+}
+
+function read_account()
+{
+    global $conn;
+
+    if (isset($_POST['username'])) 
+    {
+        $query = "SELECT * FROM account WHERE username = :username";
+        
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(':username', $_POST['username'], PDO::PARAM_STR);
+        $stmt->execute();
+    
+        if ($row = $stmt->fetch(PDO::FETCH_ASSOC)) 
+        {
+            echo json_encode($row);
+            return;
+        }
+    }
+    echo json_encode("error");
+}
+
+function update_account()
+{
+    global $conn;
+
+    if (isset($_POST['email']) && 
+        isset($_POST['password']) && 
+        isset($_POST['old_username']) && 
+        isset($_POST['new_username']) && 
+        isset($_POST['id_persona'])) 
+    {
+        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+        $query = "UPDATE Account SET email = :email, password = :password, username = :new_username, id_persona = :id_persona WHERE username = :old_username";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(':email', $_POST['email'], PDO::PARAM_STR);
+        $stmt->bindParam(':password', $password, PDO::PARAM_STR);
+        $stmt->bindParam(':old_username', $_POST['old_username'], PDO::PARAM_STR);
+        $stmt->bindParam(':new_username', $_POST['new_username'], PDO::PARAM_STR);
+        $stmt->bindParam(':id_persona', $_POST['id_persona'], PDO::PARAM_INT);
+        $stmt->execute();
+    
+        if ($stmt->rowCount() > 0) {
+            echo json_encode("success");
+            return;
+        }
+    }
+    echo json_encode("error");
+}
+
 function gestisci_attivita($id_attivita) 
 {
     global $conn;
@@ -78,8 +155,40 @@ function gestisci_attivita($id_attivita)
 }
 
 function gestisci_login() 
-{ 
-    
+{
+    if (isset($_POST['password']) && 
+        isset($_POST['username'])) 
+    {
+        global $conn;
+
+        $password = $_POST['password'];
+        $username = $_POST['username'];
+
+        // Prepariamo la query per inserire la persona
+        $query = "select * from account natural join persona where username = :username";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(':username', $username, PDO::PARAM_STR);
+        $stmt->execute();
+
+        if($row = $stmt->fetch(PDO::FETCH_ASSOC))
+        {
+            if(password_verify($password, $row['password']))
+            {
+                echo json_encode($row);
+                return;
+            }
+            else
+            {
+                echo json_encode("error");
+                return;
+            }
+        }
+        else
+        {
+            echo json_encode("error");
+            return;
+        }
+    } 
 }
 
 function gestisci_signup() 
@@ -117,7 +226,7 @@ function gestisci_signup()
         // Prepara la query
         $stmt = $conn->prepare($query);
 
-        // Associa i parametri alla query
+        // Associa i parametri alla query1
         $stmt->bindParam(':nome', $nome, PDO::PARAM_STR);
         $stmt->bindParam(':cognome', $cognome, PDO::PARAM_STR);
         $stmt->bindParam(':data_nascita', $data_nascita, PDO::PARAM_STR);
