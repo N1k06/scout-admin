a253319f0a38565c118e1a36ee82f6e78da2c61b
st10576 <st10576@iismarconipieralisi.it>
2025-05-15 06:13:01 +0000

    POST * JSON

--- src/app/persona.php
+++ src/app/persona.php
@@ -42,15 +42,18 @@
     {
         global $conn;
 
+        $POST = file_get_contents('php://input');
+        $POST = json_decode($POST, true);
+
         //Calcolo età
         $now = new DateTime();
         $today = $now->format('Y-m-d');
-        $birthday = new DateTime($_POST['data_nascita']);
+        $birthday = new DateTime($POST['data_nascita']);
         $diff = $birthday->diff($now);
         $age = $diff->y;
 
         $id_tutore = 0;
-        $aux = preg_replace('/\D/', '', $_POST['id_tutore1']); //Mi assicuro che l'id siano solo numeri
+        $aux = preg_replace('/\D/', '', $POST['id_tutore1']); //Mi assicuro che l'id siano solo numeri
 
         if($aux == "" && $age >= 18) //Se non esiste l'id tuttore e l'età è maggiore di 18 allora va bene
         {
@@ -70,7 +73,7 @@
         }
 
         $stmt = $conn->prepare("INSERT INTO Persona (nome, cognome, data_nascita, luogo_nascita, telefono, via_residenza, citta_residenza, cap_residenza, id_tutore1) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
-        $stmt->bind_param("ssssssssi", $_POST['nome'], $_POST['cognome'], $_POST['data_nascita'], $_POST['luogo_nascita'], $_POST['telefono'], $_POST['via_residenza'], $_POST['citta_residenza'], $_POST['cap_residenza'], $id_tutore);
+        $stmt->bind_param("ssssssssi", $POST['nome'], $POST['cognome'], $POST['data_nascita'], $POST['luogo_nascita'], $POST['telefono'], $POST['via_residenza'], $POST['citta_residenza'], $POST['cap_residenza'], $id_tutore);
         $stmt->execute();  
     }
 
