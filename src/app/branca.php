40d53288c17f58b37d3e95e1e0ffa58200ddcea8
st10685 <st10685@iismarconipieralisi.it>
2025-05-20 07:22:15 +0000

    Merge

---
+++ src/app/branca.php
@@ -0,0 +1,12 @@
+<?php
+require_once = "db.php";
+
+function leggi_branche() { 
+       
+}
+
+function crea_branche() {
+}
+
+function modifica_branche() {
+}
--- src/public/index.php
+++ src/public/index.php
@@ -1,7 +1,6 @@
 <?php
 require_once '../app/router.php';
 require '../app/unita.php';
-//phpinfo();
 
 $routes = [
     'GET' => [
@@ -9,11 +8,18 @@ $routes = [
         '/api/articoli/{categoria}' => 'gestisci_articoli_per_categoria',
         '/api/articoli/{categoria}/{sottocategoria}' => 'gestisci_articoli_per_sottocategoria',
         '/api/articoli/{categoria}/{sottocategoria}/{slug}' => 'gestisci_articolo',
+        '/api/branche' => 'leggi_branche', // ritorna tutte le brance
         '/api/unita' => 'read_unita'
     ],
     'POST' => [
         '/api/auth' => 'gestisci_autenticazione',
         '/api/articoli' => 'crea_articolo',
+        '/api/utenti' => 'crea_utente', //crea un utente con dati passati tramite post'
+        //'/api/branche' => 'crea_branche', //crea una nuova branch con dati passati tramite post'
+    ],
+    'PUT' => [
+        '/api/utenti/{id}' => 'modifica_utente', //modifica un utente con dati passati tramite put tutto tranne id
+        '/api/branche/{id}' => 'modifica_branche', //modifica una branch con dati passati tramite put tutto tranne id
         '/api/unita' => 'insert_unita'
         '/api/signup' => 'gestisci_signup'
         '/api/login' => 'gestisci_login'
